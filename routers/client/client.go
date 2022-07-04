66b8fdb13ec36346f533b2b08e907ad5267a742f
Nya Candy <dev@candinya.com>
2022-07-04 21:15:44 +0800

    feat: add more endpoints

--- routers/client/client.go
+++ routers/client/client.go
@@ -8,6 +8,8 @@ import (
 func Routers(g *gin.RouterGroup) {
 	r := g.Group("/")
 	r.GET("/", client.Health)
+	r.POST("/upload", client.Upload)
 	r.PUT("/upload", client.Upload)
 	r.POST("/json", client.PostJson)
+	r.PUT("/json", client.PostJson)
 }
