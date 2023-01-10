86b7d1c65b5857d6a7612baf7ebc01d0447a56b3
Gkirito <lownkid@outlook.com>
2023-01-10 16:27:43 +0800

    🔙 revert(utils/upload_to_4everland): revert

--- utils/upload_to_4everland.go
+++ utils/upload_to_4everland.go
@@ -1,17 +1,16 @@
 package utils
 
 import (
-	"fmt"
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"github.com/NaturalSelectionLabs/IPFS-Upload-Relay/global"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"io"
 	"log"
-	"net/http"
 	"strings"
-	"time"
 )
 
 var (
@@ -22,9 +21,8 @@ func prepare() (*s3.S3, error) {
 	if sess == nil {
 		var err error
 		sess, err = session.NewSession(&aws.Config{
-			Endpoint: aws.String("https://s3.us-east-2.amazonaws.com/coming-upload/"),
-			Region:   aws.String("us-east-2"),
-			LogLevel: aws.LogLevel(aws.LogDebugWithSigning),
+			Endpoint: aws.String("https://endpoint.4everland.co/"),
+			Region:   aws.String("us-west-1"),
 		})
 		if err != nil {
 			return nil, err
@@ -34,24 +32,22 @@ func prepare() (*s3.S3, error) {
 	return s3.New(sess), nil
 }
 
-func Upload2ForeverLand(r io.ReadSeeker) (string, int64, error) {
+func Upload2ForeverLand(fileBytes []byte) (string, error) {
 
 	// RandKey: file content hash
 
-	fileHash := CalcFileHash(r)
+	fileHashBytes := sha256.Sum256(fileBytes)
+	fileHash := hex.EncodeToString(fileHashBytes[:])
 
 	log.Println("New file upload request with hash: ", fileHash)
 
 	svc, err := prepare()
 	if err != nil {
-		return "", -1, err
+		return "", err
 	}
 
 	// Prepare CID
-	var (
-		cid      string
-		filesize int64
-	)
+	var cid string
 
 	// Check if already exists
 	headResp, err := svc.HeadObject(&s3.HeadObjectInput{
@@ -63,40 +59,27 @@ func Upload2ForeverLand(r io.ReadSeeker) (string, int64, error) {
 		case "NotFound":
 			// Upload file
 			_, err := svc.PutObject(&s3.PutObjectInput{
-				Body:   r,
+				Body:   bytes.NewReader(fileBytes),
 				Bucket: &global.ForeverLand_Bucket,
 				Key:    aws.String(fileHash),
 			})
 			if err != nil {
-				return "", -1, err
+				return "", err
 			} else {
 				// cid = *uploadResp.ETag // Unable to handle here, need another head
-				for i := 0; i < 5; i++ {
-					headResp, _ := svc.HeadObject(&s3.HeadObjectInput{
-						Bucket: &global.ForeverLand_Bucket,
-						Key:    aws.String(fileHash),
-					})
-					if headResp.Metadata != nil {
-						cid = *headResp.Metadata["Ipfs-Cid"]
-						filesize = *headResp.ContentLength
-						break
-					}
-					time.Sleep(1000 * time.Millisecond)
-				}
+				headResp, _ := svc.HeadObject(&s3.HeadObjectInput{
+					Bucket: &global.ForeverLand_Bucket,
+					Key:    aws.String(fileHash),
+				})
+				cid = *headResp.ETag
 			}
 		default:
-			return "", -1, err
+			return "", err
 		}
 	} else {
-		if headResp.Metadata != nil {
-			cid = *headResp.Metadata["Ipfs-Cid"]
-		}
-		filesize = *headResp.ContentLength
+		cid = *headResp.ETag
 	}
 
-	// Request once to ensure file pinned
-	go (&http.Client{}).Get(fmt.Sprintf("https://ipfs-node.coming.chat/ipfs/%s", cid))
-
-	return strings.ReplaceAll(cid, "\"", ""), filesize, nil
+	return strings.ReplaceAll(cid, "\"", ""), nil
 
 }
