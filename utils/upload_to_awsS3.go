b2099484b6e501a0636400f1273a5118daa7bad0
Gkirito <lownkid@outlook.com>
2023-01-10 16:29:29 +0800

    🌟 feat(utils/upload_to_awss3): new upload func

---
+++ utils/upload_to_awsS3.go
@@ -0,0 +1,102 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/NaturalSelectionLabs/IPFS-Upload-Relay/global"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var (
+	awsS3Session *session.Session
+)
+
+func awsS3prepare() (*s3.S3, error) {
+	if awsS3Session == nil {
+		var err error
+		awsS3Session, err = session.NewSession(&aws.Config{
+			Endpoint: aws.String("https://s3.us-east-2.amazonaws.com/coming-upload/"),
+			Region:   aws.String("us-east-2"),
+			LogLevel: aws.LogLevel(aws.LogDebugWithSigning),
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return s3.New(sess), nil
+}
+
+func UploadToAwsS3(r io.ReadSeeker) (string, int64, error) {
+
+	// RandKey: file content hash
+
+	fileHash := CalcFileHash(r)
+
+	log.Println("New file upload request with hash: ", fileHash)
+
+	svc, err := awsS3prepare()
+	if err != nil {
+		return "", -1, err
+	}
+
+	// Prepare CID
+	var (
+		cid      string
+		filesize int64
+	)
+
+	// Check if already exists
+	headResp, err := svc.HeadObject(&s3.HeadObjectInput{
+		Bucket: &global.ForeverLand_Bucket,
+		Key:    aws.String(fileHash),
+	})
+	if err != nil {
+		switch err.(awserr.Error).Code() {
+		case "NotFound":
+			// Upload file
+			_, err := svc.PutObject(&s3.PutObjectInput{
+				Body:   r,
+				Bucket: &global.ForeverLand_Bucket,
+				Key:    aws.String(fileHash),
+			})
+			if err != nil {
+				return "", -1, err
+			} else {
+				// cid = *uploadResp.ETag // Unable to handle here, need another head
+				for i := 0; i < 5; i++ {
+					headResp, _ := svc.HeadObject(&s3.HeadObjectInput{
+						Bucket: &global.ForeverLand_Bucket,
+						Key:    aws.String(fileHash),
+					})
+					if headResp.Metadata != nil {
+						cid = *headResp.Metadata["Ipfs-Cid"]
+						filesize = *headResp.ContentLength
+						break
+					}
+					time.Sleep(1000 * time.Millisecond)
+				}
+			}
+		default:
+			return "", -1, err
+		}
+	} else {
+		if headResp.Metadata != nil {
+			cid = *headResp.Metadata["Ipfs-Cid"]
+		}
+		filesize = *headResp.ContentLength
+	}
+
+	// Request once to ensure file pinned
+	go (&http.Client{}).Get(fmt.Sprintf("https://ipfs-node.coming.chat/ipfs/%s", cid))
+
+	return strings.ReplaceAll(cid, "\"", ""), filesize, nil
+
+}
