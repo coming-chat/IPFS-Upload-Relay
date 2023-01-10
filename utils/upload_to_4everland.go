300460b8d7c8da2c4f8ac2ab7c99cc1fa33095da
Gkirito <lownkid@outlook.com>
2023-01-10 16:58:14 +0800

    🔙 revert(utils/upload_to_4overland): revert to new

--- utils/upload_to_4everland.go
+++ utils/upload_to_4everland.go
@@ -1,15 +1,15 @@
 package utils
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"encoding/hex"
+	"fmt"
 	"github.com/NaturalSelectionLabs/IPFS-Upload-Relay/global"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"io"
 	"log"
+	"net/http"
 	"strings"
 )
 
@@ -32,22 +32,25 @@ func prepare() (*s3.S3, error) {
 	return s3.New(sess), nil
 }
 
-func Upload2ForeverLand(fileBytes []byte) (string, error) {
+func Upload2ForeverLand(r io.ReadSeeker) (string, int64, string, error) {
 
 	// RandKey: file content hash
 
-	fileHashBytes := sha256.Sum256(fileBytes)
-	fileHash := hex.EncodeToString(fileHashBytes[:])
+	fileHash := CalcFileHash(r)
 
 	log.Println("New file upload request with hash: ", fileHash)
 
 	svc, err := prepare()
 	if err != nil {
-		return "", err
+		return "", -1, "", err
 	}
 
 	// Prepare CID
-	var cid string
+	var (
+		cid         string
+		filesize    int64
+		contentType string
+	)
 
 	// Check if already exists
 	headResp, err := svc.HeadObject(&s3.HeadObjectInput{
@@ -59,12 +62,12 @@ func Upload2ForeverLand(fileBytes []byte) (string, error) {
 		case "NotFound":
 			// Upload file
 			_, err := svc.PutObject(&s3.PutObjectInput{
-				Body:   bytes.NewReader(fileBytes),
+				Body:   r,
 				Bucket: &global.ForeverLand_Bucket,
 				Key:    aws.String(fileHash),
 			})
 			if err != nil {
-				return "", err
+				return "", -1, "", err
 			} else {
 				// cid = *uploadResp.ETag // Unable to handle here, need another head
 				headResp, _ := svc.HeadObject(&s3.HeadObjectInput{
@@ -72,14 +75,21 @@ func Upload2ForeverLand(fileBytes []byte) (string, error) {
 					Key:    aws.String(fileHash),
 				})
 				cid = *headResp.ETag
+				filesize = *headResp.ContentLength
+				contentType = *headResp.ContentType
 			}
 		default:
-			return "", err
+			return "", -1, "", err
 		}
 	} else {
 		cid = *headResp.ETag
+		filesize = *headResp.ContentLength
+		contentType = *headResp.ContentType
 	}
 
-	return strings.ReplaceAll(cid, "\"", ""), nil
+	// Request once to ensure file pinned
+	go (&http.Client{}).Get(fmt.Sprintf("https://4everland.io/ipfs/%s", cid))
+
+	return strings.ReplaceAll(cid, "\"", ""), filesize, contentType, nil
 
 }
