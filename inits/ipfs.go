c93db1d04a100f27b9967b63f93473c7509d282c
Gkirito <lownkid@outlook.com>
2023-01-11 18:13:45 +0800

    🌟 feat(inits): init new ipfs fields

--- inits/ipfs.go
+++ inits/ipfs.go
@@ -12,6 +12,18 @@ func Ipfs() error {
 	if !exist {
 		return fmt.Errorf("env virable IPFS_URL not found")
 	}
+	global.IPFS_UPLOAD_URL, exist = os.LookupEnv("IPFS_UPLOAD_URL")
+	if !exist {
+		return fmt.Errorf("env virable IPFS_UPLOAD_URL not found")
+	}
+	global.PROJECT_ID, exist = os.LookupEnv("PROJECT_ID")
+	if !exist {
+		return fmt.Errorf("env virable PROJECT_ID not found")
+	}
+	global.PROJECT_SECRET, exist = os.LookupEnv("PROJECT_SECRET")
+	if !exist {
+		return fmt.Errorf("env virable PROJECT_SECRET not found")
+	}
 
 	return nil
 }
